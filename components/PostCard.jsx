351bc360a80263f64562921e4bb86a90f21ed9e2
LAPTOP-9QIA4U65\PC <ngocthi2932001@gmail.com>
2021-11-08 20:21:53 +0700

    update changes

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -67,7 +67,7 @@ const PostCard = ({post}) => {
         <div className="text-center">
           <Link href={`/post/${post.slug}`}>
             <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
-              Continue Readingr
+              Continue Readingsssss Button
             </span>
           </Link>
         </div>
