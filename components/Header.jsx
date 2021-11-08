82a799f4f5308ba098e98fa7b7f12140ecff8086
LAPTOP-9QIA4U65\PC <ngocthi2932001@gmail.com>
2021-11-08 19:39:26 +0700

    update bt cho anh nhan

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Link from "next/link";
+import { getCategories } from '../services';
 
 const Header = () => {
-    
+  const [categories, setcatelogies] = React.useState([]);
+  React.useEffect(() => {
+    getCategories().then((data)=>setcatelogies(data));
+  },[])
     return (
       <div className="container mx-auto px-10 mb-8">
         <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -14,13 +18,13 @@ const Header = () => {
             </a>
           </div>
           <div className="hidden md:float-left md:contents">
-            {/* {categories.map((category, index) => (
+            {categories.map((category, index) => (
               <Link key={index} href={`/category/${category.slug}`}>
                 <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                   {category.name}
                 </span>
               </Link>
-            ))} */}
+            ))}
           </div>
         </div>
       </div>
