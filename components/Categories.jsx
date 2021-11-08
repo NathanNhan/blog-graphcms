41e36f7fc2ebbba5558f06187a4e0b320f476659
Trong Nhan <trongnhan@gkxim.com>
2021-11-08 20:12:38 +0700

    sadasd

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 
 
+
 const Categories = () => {
   
-
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
